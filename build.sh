86220f4ee436c59eccfbcc1bf725ce2fbb50aaa6
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2022-02-07 15:48:24 +0100

    Favicon, caching, ...

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-test -d build || mkdir build
-id=$(printf %x $(date +%s))
+rm -rf build/
+mkdir build
+id=$(printf "%x" $(date +%s))
 
-minify style.css > build/style.$(id).css
+cp favicon-96x96.png build/favicon-96x96.$id.png
 
-uglifyjs worker.js --mangle --toplevel --compress --output build/worker.$(id).js --source-map
+minify style.css > build/style.$id.css
 
-uglifyjs script.js --mangle --toplevel --compress --output build/script.$(id).js --source-map
-sed "s/worker.js/worker.$id.js"
+uglifyjs worker.js --mangle --toplevel --compress --output build/worker.$id.js --source-map
+
+uglifyjs script.js --mangle --toplevel --compress --output build/script.$id.js --source-map
+sed -i "s/worker.js/worker.$id.js/" build/script.$id.js
 
 minify index.html > build/index.html
-sed "s/script.js/script.$id.js;s/style.css/style.$id.css"
+sed -i "s/script.js/script.$id.js/;s/style.css/style.$id.css/;s/favicon-96x96.png/favicon-96x96.$id.png/" build/index.html
 
 cp htaccess build/.htaccess
-rsync -avP build/* uber:html/calendar/
+ssh uber rm -f html/calendar/*
+rsync -avP --delete build/* uber:html/calendar/
